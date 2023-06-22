776a8733167f7e56b7745053e4c690b4f5ee9e07
saimdemir <133411734+saimdemir@users.noreply.github.com>
2023-06-22 16:44:49 +0300

    Add files via upload

---
+++ argentina/argentina.js
@@ -0,0 +1,5 @@
+/*const ball = document.querySelector('.fa-solid fa-sun');
+const items = document.querySelectorAll('.navbar,.sidebar,.font-aw');
+ball.addEventListener(click,function(){
+    ball.backgroundcolor.white
+})*/
